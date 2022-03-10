a0dd534465bc20e00beb15e094dbe365f896d303
lewiEyasu <eyasulewi@gmail.com>
2022-03-10 21:11:40 +0300

    commit

--- github_.sh
+++ github_.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
 git commit -m "commit"
-git push https://lewiEyasu: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@github.com/lewiEyasu/alx-low_level_programming.git --all
+git push -u origin main
