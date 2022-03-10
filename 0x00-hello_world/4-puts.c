63d1f1e8a8449f32a5c807382e5a146926732642
eyasu Shiferaw <51745710+lewiEyasu@users.noreply.github.com>
2022-03-10 23:00:02 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-/**
- * my_function - This is a description
- */
+
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*
-	* Return: 1 if the number is positive. 0 otherwise
-        */
 	return (0);
 }
